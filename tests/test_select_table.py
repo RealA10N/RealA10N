3b7ade2ba47ee0d3a6abb4542cd308f8c029ff58
Alon (PC) <downtown2u@gmail.com>
2021-01-14 09:23:00 +0200

    ✅ Fixed broken tests

--- tests/test_select_table.py
+++ tests/test_select_table.py
@@ -4,8 +4,8 @@ import json
 
 import requests
 
+import decoration_types
 from select_table import (
-    DecorationType,
     DecorationTable,
     DecorationTableCell,
 )
@@ -15,35 +15,16 @@ class TestDecorationTypes:
 
     SELECT_TABLE_NAME = 'select_table.html'
 
-    def test_types_general_config(self,):
-        """ Tests the main general configuration file, and asserts that
-        everything is ok and it contains all of the needed fields. """
-
-        data_path = os.path.join(
-            DecorationType.GENERAL_CONFIG_FOLDER,
-            DecorationType.GENERAL_CONFIG_FILENAME
-        )
-
-        with open(data_path, 'r') as file:
-            data = json.load(file)
-
-        for type_data in data:
-            assert 'type' in type_data, "All types must contain the `type` field"
-
-            if 'label' in type_data:
-                assert 'text' in type_data['label']
-                assert 'color' in type_data['label']
-
     def test_types_labels(self,):
         """ Tests that each configuration file (excluding the main one)
         is ok and contains all of the needed fields. """
 
-        for dec_type_name in DecorationType.avaliable_types():
-            dec_type = DecorationType(dec_type_name)
+        for dec_type in decoration_types.TYPES_TABLE:
+            dec_obj = decoration_types.TYPES_TABLE[dec_type]
 
-            if dec_type.is_label():
+            if dec_obj.is_label():
 
-                url = dec_type.label_url()
+                url = dec_obj.label_url()
                 response = requests.get(url)
 
                 assert response.status_code == 200, f"Invalid url, label error {response.status_code}"
