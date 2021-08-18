fca54f3b62c9625ce2e38ffbdf19722d7b941f8d
Alon (PC) <downtown2u@gmail.com>
2021-08-18 23:55:11 +0300

    ✨ Added `waitress` production server support

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import io
 from flask import Flask, send_file
 from art import GitHubBanner
 
-app = Flask(__name__)
-dynamicbanner = GitHubBanner('github-data.json')
+
+def start():
+    app = Flask(__name__)
+    dynamicbanner = GitHubBanner('github-data.json')
+
+    @app.route('/github-banner.png')
+    def github_banner():
+        arr = io.BytesIO()
+
+        img = next(dynamicbanner)
+        img.save(arr, format='png')
+
+        arr.seek(0)
+        return send_file(arr, mimetype='image/png')
+
+    return app
 
 
-@app.route('/github-banner.png')
-def github_banner():
-    arr = io.BytesIO()
-
-    img = next(dynamicbanner)
-    img.save(arr, format='png')
-
-    arr.seek(0)
-    return send_file(arr, mimetype='image/png')
+if __name__ == '__main__':
+    from waitress import serve
+    serve(start())
