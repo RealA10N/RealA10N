56bef7d2d5570e8dbee024e65dd76c4708825687
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-15 13:40:05 +0200

    🚀 Deployed as Azure Webapp instead of Heroku

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <div align="center">
-    <image src="https://dynamicbanner.herokuapp.com/github-banner.png" width="1000%"/>
+    <image src="https://github-banner.azurewebsites.net/github-banner.png" width="1000%"/>
 </div>
--- app.py
+++ app.py
@@ -2,31 +2,24 @@ import io
 from flask import Flask, send_file, make_response
 from art import GitHubBanner
 
-
-def start():
-    app = Flask(__name__)
-    dynamicbanner = GitHubBanner('github-data.json')
-
-    @app.route('/github-banner.png')
-    def github_banner():
-        arr = io.BytesIO()
-
-        img = next(dynamicbanner)
-        img.save(arr, format='png')
-
-        arr.seek(0)
-
-        re = make_response(send_file(arr, mimetype='image/png'))
-        re.headers['Cache-Control'] = ', '.join((
-            'max-age=0',
-            'no-cache',
-            'no-store',
-            'must-revalidate',
-        ))
-        return re
-
-    return app
+app = Flask(__name__)
+dynamicbanner = GitHubBanner('github-data.json')
 
 
-if __name__ == '__main__':
-    start().run()
+@app.route('/github-banner.png')
+def github_banner():
+    arr = io.BytesIO()
+
+    img = next(dynamicbanner)
+    img.save(arr, format='png')
+
+    arr.seek(0)
+
+    re = make_response(send_file(arr, mimetype='image/png'))
+    re.headers['Cache-Control'] = ', '.join((
+        'max-age=0',
+        'no-cache',
+        'no-store',
+        'must-revalidate',
+    ))
+    return re
